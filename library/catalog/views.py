c32c98ab94faafe73c1f641008b1538cc1ed2cc8
kang <kmoon601>
2020-05-30 03:10:48 +0900

    edit6

--- library/catalog/views.py
+++ library/catalog/views.py
@@ -8,18 +8,15 @@ def index(request):
     # Generate counts of some of the main objects
     num_books = Book.objects.all().count()
     num_instances = BookInstance.objects.all().count()
-    
-    # Available books (status = 'a')
-    num_instances_available = BookInstance.objects.filter(status__exact='a').count()
-    
-    # The 'all()' is implied by default.    
+    num_instances_available = BookInstance.objects.filter(status__exact='a').count()  
     num_authors = Author.objects.count()
-    
+    num_authors = Author.objects.count()
     context = {
         'num_books': num_books,
         'num_instances': num_instances,
         'num_instances_available': num_instances_available,
         'num_authors': num_authors,
+        'num_visits': num_visits,
     }
 
 from django.views import generic
